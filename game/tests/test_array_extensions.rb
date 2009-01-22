dec0c685f232d66d561d31464017095e482b09de
Martin Carel <cawel00@yahoo.com>
2009-01-22 18:55:38 -0500

    added tests for Array#common_element

---
+++ game/tests/test_array_extensions.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+require 'rubygems'
+require 'context'
+require File.join(File.dirname(__FILE__), '../array_extensions.rb')
+
+class TestArrayExtensions < Test::Unit::TestCase
+
+	context 'When the array is empty' do
+		it "should return nil" do
+			assert_nil [].common_element
+		end
+	end
+
+	context 'When the array contains the same elements' do
+		it 'should return this element' do
+			assert 1, [1,1,1].common_element
+		end
+	end
+
+	context 'When the array does not contain the same elements' do
+		it 'should return nil' do
+			assert 1, [1,2,3].common_element
+		end
+	end
+
+end
