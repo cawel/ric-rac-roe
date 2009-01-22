b45e891c9036a63d865fdfd226b6768efad459d7
Martin Carel <cawel00@yahoo.com>
2009-01-22 17:24:56 -0500

    automated file list in suite runner added contexts in test_game

--- game/tests/test_game.rb
+++ game/tests/test_game.rb
@@ -1,4 +1,6 @@
 require 'test/unit'
+require 'rubygems'
+require 'context'
 require File.join(File.dirname(__FILE__), '../game.rb')
 
 
@@ -10,22 +12,46 @@ end
 
 class TestGame < Test::Unit::TestCase
 
-	def setup
-		@game = Game.new
+	context 'Before the game begins' do
+
+		before do
+			@game = Game.new
+		end
+
+		test "should be player with crosses as first_player" do
+			assert_equal(@game.current_player, Grid::CROSS)
+		end
+
+		test "all squares in matrix should be nil" do
+			@game.squares.each {|row| assert row.compact.empty?}
+		end
+
 	end
 
-	def test_should_be_player_with_crosses_as_first_player
-		assert_equal(@game.current_player, Grid::CROSS)
+
+	context 'During the course of the game' do
+
+		before do
+			@game = Game.new
+			@mockGameplay = MockGameplay.new
+		end
+
+		test "should toggle player after a move" do
+			@mockGameplay.position = Position.new(0,0)
+			@game.play @mockGameplay
+			assert_equal(@game.current_player, Grid::NAUGHT)
+
+			@mockGameplay.position = Position.new(1,0)
+			@game.play @mockGameplay
+			assert_equal(@game.current_player, Grid::CROSS)
+		end
+
+		test "should modify squares matrix after move" do
+			@mockGameplay.position = Position.new(0,0)
+			@game.play @mockGameplay
+			assert_equal('X', @game.squares[0][0])
+		end
+
 	end
 
-	def test_should_toggle_player_after_playing
-		mockGameplay = MockGameplay.new
-		mockGameplay.position = Position.new(0,0)
-		@game.play mockGameplay
-		assert_equal(@game.current_player, Grid::NAUGHT)
-
-		mockGameplay.position = Position.new(1,0)
-		@game.play mockGameplay
-		assert_equal(@game.current_player, Grid::CROSS)
-	end
 end
--- game/tests/test_suite.rb
+++ game/tests/test_suite.rb
@@ -1,6 +1,10 @@
-require 'test/unit'
+tmp_cd = Dir.pwd
 
-require File.join(File.dirname(__FILE__), 'test_game.rb')
-require File.join(File.dirname(__FILE__), 'test_grid.rb')
-require File.join(File.dirname(__FILE__), 'test_position.rb')
+Dir.chdir File.dirname( __FILE__)
+files =  Dir["test_*"]
+files.delete File.basename(__FILE__)
+files.each do |test_file|
+	require  test_file
+end
 
+Dir.chdir tmp_cd
