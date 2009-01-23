c5f9f6ebb8b994654a38ec78dbd288e421723c37
Martin Carel <cawel00@yahoo.com>
2009-01-23 12:35:25 -0500

    added 'new game' link when game is over

--- shoes/app.rb
+++ shoes/app.rb
@@ -9,33 +9,32 @@ Shoes.app(:title => "Ric-rac-roe", :width => 630, :height => 670) do
 		3.times {@images << Array.new(3)}
 	end
 
-	init_game
-
-	# elements layout
-	@gameplay = stack do
-		(0..2).each do |row|
-			flow do
-				(0..2).each do |col|
-					@images[col][row] = image "images/empty.gif", :margin => 5
-					@images[col][row].click { on_click(col, row) } 
+	def draw_elements
+		stack do
+			(0..2).each do |row|
+				flow do
+					(0..2).each do |col|
+						@images[col][row] = image "images/empty.gif", :margin => 5
+						@images[col][row].click { on_click(col, row) } 
+					end
 				end
 			end
+			@status_line = stack do
+				@status = para "Let the game begin!", :stroke => white
+			end
 		end
-		@status = para "Let the game begin!", :stroke => white
-		#@new_game = button "new game" do
-			#new_game
-		#end
 	end
 
+	init_game
+	@gameplay = draw_elements
+	
 
 	### helper methods
 
 	def new_game
 		init_game
-		@gameplay.clear
-		# TODO: how do I redraw my gameplay?
+		@gameplay.clear{ draw_elements } 
 	end
-
 
 	def on_click x, y
 		play(x, y)
@@ -54,12 +53,11 @@ Shoes.app(:title => "Ric-rac-roe", :width => 630, :height => 670) do
 	end
 
 	def end_game
-		if @game.winner.nil?
-			@status.text = "Draw!"
-		else
-			@status.text = "Player with #{@game.winner}'s won!"
-		end
+		end_game_msg = @game.winner.nil? ?  "Draw! " : "Player with #{@game.winner}'s won! "
+		link = link("New game") { new_game }
+		@status_line.clear { para end_game_msg, link , "?", :stroke => white }
 
+		#disable click events on all squares
 		(0..2).each do |row|
 			(0..2).each do |col|
 				@images[col][row].click {}
