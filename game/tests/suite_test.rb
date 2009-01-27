6c3ff24e8d5e1254c0677dea9bfea6f43c61e085
Martin Carel <cawel00@yahoo.com>
2009-01-27 13:16:55 -0500

    improved view testing, and tests in general

---
+++ game/tests/suite_test.rb
@@ -0,0 +1,13 @@
+# tests suite runner
+# It takes all tests in current directory (except for this file) and run them
+
+tmp_cd = Dir.pwd
+
+Dir.chdir File.dirname( __FILE__)
+files =  Dir["test_*"]
+files.delete File.basename(__FILE__)
+files.each do |test_file|
+	require  test_file
+end
+
+Dir.chdir tmp_cd
